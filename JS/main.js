817f1b761318c52dffc5a6ced62d475e52ec7313
Diana Salazar <dayanasoll@hotmail.com>
2023-05-29 18:22:27 -0300

    agregando footer

--- JS/main.js
+++ JS/main.js
@@ -1,62 +1,74 @@
-const coleccionBBDD = firebase.firestore().collection("BBDD"); 
-let dataBBDD=[]
+let inputSearch = document.getElementById("buscador");
+
+
+
+
+const coleccionBBDD = firebase.firestore().collection("BBDD");
+let dataBBDD = []
 coleccionBBDD.get()
-  .then((results) => {
-    console.log(results)
-    const data = results.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    dataBBDD.push(...data)
-    console.log("Toda data en la coleccion 'BBDD' ", data); 
- });
+    .then((results) => {
+        console.log(results)
+        const data = results.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+        dataBBDD.push(...data)
+        console.log("Toda data en la coleccion 'BBDD' ", data);
+    });
 console.log(dataBBDD)
 
-(function () {
-    const listElements = document.querySelectorAll('.menu-item-show');
-    const list = document.querySelector('.menu-links');
-    const menu = document.querySelector('.menu-hamburguer');
-    const colleccionBBDD = firebase.firestore().collection("BBDD");
+    (function () {
+        const listElements = document.querySelectorAll('.menu-item-show');
+        const list = document.querySelector('.menu-links');
+        const menu = document.querySelector('.menu-hamburguer');
+        const colleccionBBDD = firebase.firestore().collection("BBDD");
 
-    const addClick = () => {
-        listElements.forEach(element => {
-            element.addEventListener('click', () => {
-                let subMenu = element.children[1];
-                let height = 0;
-                element.classList.toggle('menu-item-active');
+        const addClick = () => {
+            listElements.forEach(element => {
+                element.addEventListener('click', () => {
+                    let subMenu = element.children[1];
+                    let height = 0;
+                    element.classList.toggle('menu-item-active');
 
-                if (subMenu.clientHeight === 0) {
-                    height = subMenu.scrollHeight;
-                }
-                subMenu.style.height = `${height}px`;
+                    if (subMenu.clientHeight === 0) {
+                        height = subMenu.scrollHeight;
+                    }
+                    subMenu.style.height = `${height}px`;
+                });
             });
-        });
-    }
+        }
 
-    const deleteStyleHeight = () => {
-        listElements.forEach(element => {
-            if (element.children[1].getAttribute('style')) {
-                element.children[1].removeAttribute('style');
-                element.classList.remove('menu-item-active');
+        const deleteStyleHeight = () => {
+            listElements.forEach(element => {
+                if (element.children[1].getAttribute('style')) {
+                    element.children[1].removeAttribute('style');
+                    element.classList.remove('menu-item-active');
+                }
+            });
+        }
+
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth > 800) {
+                deleteStyleHeight();
+                if (list.classList.contains('menu-links-show'))
+                    list.classList.remove('menu-links-show');
+            } else {
+                addClick();
             }
         });
-    }
 
-
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 800) {
-            deleteStyleHeight();
-            if (list.classList.contains('menu-links-show'))
-                list.classList.remove('menu-links-show');
-        } else {
+        if (window.innerWidth <= 800) {
             addClick();
         }
-    });
+        menu.addEventListener('click', () => list.classList.toggle('menu-links-show'));
+    })();
 
-    if (window.innerWidth <= 800) {
-        addClick();
-    }
-    menu.addEventListener('click', () => list.classList.toggle('menu-links-show'));
-})();
+// Search
 
+inputSearch.addEventListener("keyup", function (eventos) {
+    const datoInput = eventos.target.value;
+    search = datoInput.trim().toLowerCase();
+    filtrosCombinados();
 
+})
