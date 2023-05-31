8d368e54de721f604e338a90fa8254dd3765657d
AmeCF3 <aajcf3@gmail.com>
2023-05-30 22:25:21 -0300

    home dinámico

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,7 @@ let bodegas = document.getElementById("info-cellers")
 let vinosContainer = document.getElementById("vinos-container")
 let allWines = document.getElementById("allWines")
 let cardsShop = document.getElementsByClassName("card-container-general1")
-let history = document.getElementById("history")
+let history =document.getElementById("history")
 
 var buttonNav = [];
 let dataVinos = [];
@@ -13,21 +13,24 @@ let dataVinos = [];
 // LLAMADO A LA API
 const coleccionVinos = firebase.firestore().collection("BBDD");
 
-coleccionVinos.get()
-    .then((results) => {
-        const data = results.docs
-            .filter((doc) => doc.data().Category === "vinos")
-            .map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-        dataVinos.push(...data);
-    })
-    .catch((error) => {
-        console.error("Error al obtener los datos:", error);
-    });
+function getDataVinos() {
+    coleccionVinos.get()
+        .then((results) => {
+            dataVinos = results.docs
+                .filter((doc) => doc.data().Category === "vinos")
+                .map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+            console.log(dataVinos);
+        })
+        .catch((error) => {
+            console.error("Error al obtener los datos:", error);
+        });
+}
 
-console.log(dataVinos)
+// Llamar a la función para obtener los vinos automáticamente
+getDataVinos();
 
 
 //FUNCIÓN PARA CAMBIAR DE PÁGINA
@@ -69,7 +72,8 @@ function imprimir(id) {
             video.style.display = "none";
             form.innerHTML = ""
             bodegas.innerHTML = ""
-            vinosContainer.style.display = "none";
+            vinosContainer.style.display = "block";
+            positionName.innerHTML = "Todos nuestros vinos"
             allWines.style.display = "flex"
             history.innerHTML = ""
             print(dataVinos);
@@ -80,21 +84,24 @@ function imprimir(id) {
             video.style.display = "none";
             form.innerHTML = ""
             bodegas.innerHTML = ""
-            vinosContainer.style.display = "none";
+            vinosContainer.style.display = "block";
+            positionName.innerHTML = "Nuestros vinos tintos"
             history.innerHTML = ""
+            allWines.style.display = "flex"
             redWines = dataVinos.filter((vino) => vino.Variety === "Cabernet Sauvignon" || vino.Variety === "Malbec");
             print(redWines);
             console.log(redWines);
             apagardetalles ();
             window.history.replaceState(null, null, window.location.origin + "/index.html?time=shop/vinos-rojos");
-
             break;
         case "white-wine":
             video.style.display = "none";
             form.innerHTML = ""
             bodegas.innerHTML = ""
-            vinosContainer.style.display = "none";
+            vinosContainer.style.display = "block";
+            positionName.innerHTML = "Nuestros vinos blancos"
             history.innerHTML = ""
+            allWines.style.display = "flex"
             whiteWines = dataVinos.filter((vino) => vino.Variety === "Chardonnay");
             print(whiteWines)
             apagardetalles ();
@@ -107,9 +114,8 @@ function imprimir(id) {
             allWines.style.display = "none"
             vinosContainer.style.display = "none";
             history.innerHTML = ""
-            apagardetalles ();
-            printForm();
 
+            printForm();
             break;
         default:
             window.history.replaceState(null, null, window.location.origin + "/index.html?time=home");
@@ -117,10 +123,14 @@ function imprimir(id) {
             bodegas.innerHTML = ""
             allWines.innerHTML = ""
             vinosContainer.style.display = "block";
-            allWines.style.display = "none"
+            allWines.style.display = "flex"
             history.innerHTML = ""
-            apagardetalles ();
+
+
             video.style.display = "block"
+            positionName.innerHTML = "Nuestros más económicos"
+            const vinosFiltrados = dataVinos.filter((vino) => vino.Price <= 1500);
+            print(vinosFiltrados)
     }
 }
 
@@ -170,7 +180,6 @@ function print(vinosArray) {
  
 
 }
-
 
 // FUNCIÓN IMPRIMIR FORMULARIO
 function printForm() {
@@ -439,3 +448,23 @@ function printHistory() {
          </div>
     `
 }
+
+// BUSCADOR
+var inputSearch = document.getElementById("buscador");
+
+inputSearch.addEventListener("keyup", function (event) {
+    var datoInput = event.target.value;
+    var datosOrdenados = datoInput.trim().toLowerCase();
+    var filtrado = dataVinos.filter(vino => vino.Name.toLowerCase().includes(datosOrdenados));
+
+    if (filtrado.length === 0) {
+        // No se encontraron resultados
+        allWines.innerHTML =
+            `<div class="ceroSearch">
+                <img class="imgCero" src="./multimedia/nofoundmin.png" alt="SinResultados">
+            </div>`;
+    } else {
+        // Imprimir los resultados filtrados
+        print(filtrado);
+    }
+});
